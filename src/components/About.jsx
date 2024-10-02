0ed91f5f5109db98792c80e42dd021dd842eb7f5
Firdosh Siddique <siddiquefirdosh12@gmai.com>
2024-10-02 09:53:42 +0530

    Done

---
+++ src/components/About.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Firdosh Siddique, a passionate Web developer with a keen eye for MERN
+          Stack and DevOps Er. . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          [MCA, Dr APJ ABdul Kalam Technical University Lucknow, 2023] [AWS: clound Pracititonar, Udemy & College Training, 2023 ] [DevOpsin AWS: code, Build & Test in 2023]
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in C/C++, Python Java Programming Language, Experienced with MERN, AWS cloud, AWS DevOps Git and Github .. Etc..Tools. 
+          Excellent problem-solving skills Effective communicator and collaborator.
+        </span>
+        <br />
+        <br />
+       
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+        My mission is to leverage my expertise in software development and cloud engineering to create innovative, scalable solutions that drive efficiency and enhance user experience. I aim to empower organizations with robust, reliable technologies that foster growth and success.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
